51cf0becbe928a959be999b735c49786ad542d81
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2026-01-26 15:13:54 +0700

    feat(config): add user data encryption configuration to .env.example and .gitignore

---
+++ root_src/src/jobs/user-encryption/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * User Encryption Services Barrel Export
+ *
+ * @module UserEncryption/Services
+ * @version 1.0.0
+ */
+
+export * from './encryption.service';
+export * from './user-encryption-job.service';
